adea67329bbf6044e9f7fd7a26c54561264335ac
Halimdv <deville.halim@gmail.com>
2016-08-11 11:51:06 +0200

    Add Pixel model and controller
    (Pour controller les pages en rapport avec la création et la
    visualisation de pixel)

---
+++ app/Controller/PixelController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Controller;
+
+use \W\Controller\Controller;
+use \Model\UserModel;
+class PixelController extends Controller
+{
+
+}
--- app/routes.php
+++ app/routes.php
@@ -6,5 +6,6 @@
 		['GET', '/user/[i:id]', "User#affiche", "user_show"],
 		['GET|POST', '/inscription', 'User#inscription', 'user_inscription'],
 		['GET|POST', '/login', 'Security#login', 'security_login'],
-		['GET', '/create', 'User#create', 'user_create']
+		['GET', '/create', 'User#create', 'user_create'],
+		
 	);
