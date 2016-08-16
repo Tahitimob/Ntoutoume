88dd7b9c8939a478330f9405f20fe04991bb2901
Aequin0x <thibaut.desrame@gmail.com>
2016-08-16 14:22:24 +0200

    Changement inté
    *

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -1,125 +1,21 @@
 <?php $this->layout('layout', ['title' => "Pix'Hell"]) ?>
 
 <?php $this->start('main_content') ?>
-<div id="home" class="">
+<div id="home" class="mef">
+  <div class="vertical-line"></div>
 	<div class="container">
-		<div class="jumbotron">
-			<img src="assets/img/logo.png">
-	  		
-	   			<h1>Welcome on Pix'Hell</h1>
-	   			<p>Viens on est bien Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, nulla tempora, quis dicta ipsam, aperiam delectus quaerat praesentium nihil perferendis quas possimus. Mollitia magni exercitationem autem eum, aliquid atque laudantium.</p>
-	  		
-	  		<p><a class="btn btn-primary btn-lg" href="#" role="button">Créer</a></p>
-		</div>
-	</div>
-</div>
-<div id="thumb">
-	<div class="container">
-	<div class="row">
-		<div class="text-center">
-			<h2>Voici quelques créations !</h2>
-		</div>
- 		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-  		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-  		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-	</div>
-	<div class="row">
- 		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-  		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-  		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-	</div>
-	<div class="row">
- 		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-  		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-  		<div class="col-xs-6 col-md-3">
-    	<a href="#" class="thumbnail">
-      		<img src="http://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg" alt="...">
-    	</a>
-  		</div>
-	</div>
-</div>
-</div>
-<div id ="contact" class="contact">
+    <h2 class="title">WELCOME</h2>
+    <h2 class="title">ON</h2>
+    <h1 class="title text-center">PIX'HELL</h1>
 
-  <div class="container">
-	<form class="form-horizontal">
-	  	<div class="form-group">
-	    		<label for="inputNom" class="col-sm-2 control-label">Nom :</label>
-	    	<div class="col-sm-8">
-	      		<input type="text" class="form-control" id="inputNom" placeholder="Nom">
-	    	</div>
-	  	</div>
-  		<div class="form-group">
-    		<label for="inputPrenom" class="col-sm-2 control-label">Prenom :</label>
-    		<div class="col-sm-8">
-      			<input type="text" class="form-control" id="inputPrenom" placeholder="Prenom">
-    		</div>
-  		</div>
-  		<div class="form-group">
-    		<label for="inputMail" class="col-sm-2 control-label">Email :</label>
-    		<div class="col-sm-8">
-      			<input type="email" class="form-control" id="inputMail" placeholder="Email">
-    		</div>
-  		</div>
-	  	<div class="form-group">
-	    <div class="col-sm-offset-2 col-sm-8">
-	      <button type="submit" class="btn btn-default">Envoyer</button>
-	    </div>
-	    </div>
-	</form>
-	</div>
-
+    <hr class="hr">
+    <br>
+    <div class="godown">
+      <a href="<?= $this->url('pixel_create') ?>">
+        <p class="glyphicon glyphicon-chevron-down"></p>
+      </a>
+    </div>
+  </div>
+  
 </div>
 <?php $this->stop('main_content') ?>
-
-
-<!-- 	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p> -->
--- app/Views/layout.php
+++ app/Views/layout.php
@@ -7,9 +7,12 @@
 	
 	<link rel="stylesheet" href="<?= $this->assetUrl('vendor/bootstrap/dist/css/bootstrap.min.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+
+	<!-- Google Font -->
+	<link href='https://fonts.googleapis.com/css?family=Orbitron:400,900' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse navbar-css">
+	<nav class="navbar navbar-default navbar-fixed-left">
 	  	<div class="container-fluid">
 	    <!-- Brand and toggle get grouped for better mobile display -->
 	    <div class="navbar-header">
@@ -20,40 +23,82 @@
 	        <span class="icon-bar"></span>
 	      </button>
 	      <a class="navbar-brand" href="<?= $this->url('default_home') ?>">
-			<img class="brandStyle" alt="Logo pix'hell" src="assets/img/logo50px.png">
+			<img class="brandStyle" alt="Logo pix'hell" src="<?= $this->assetUrl('img/pixhell.png')?>">
 	      </a>
 	    </div>
 
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <ul class="nav navbar-nav navbar-right">
-	        <li><a href="<?= $this->url('default_home') ?>">Accueil</a></li>
-			<li><a href="<?= $this->url('user_inscription') ?>">Inscription</a></li>
-			<li><a href="<?= $this->url('security_login') ?>">Login</a></li>
-			<li><a href="<?= $this->url('pixel_create') ?>">Création</a></li>
+	      <ul class="nav navbar-nav item-position">
+	        <li><a href="<?= $this->url('default_home') ?>">¬Accueil</a></li>
+	        <br>
+			<li><a href="<?= $this->url('user_inscription') ?>">¬Inscription</a></li>
+			<br>
+			<li><a href="<?= $this->url('security_login') ?>">¬Login</a></li>
+			<br>
+			<li><a href="<?= $this->url('pixel_create') ?>">¬Création</a></li>
+			<br>
 	      </ul>
 	    </div><!-- /.navbar-collapse -->
+	    <div class="contact" id="contact">
+	    	<!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary btn-lg btn-contact" data-toggle="modal" data-target="#myModal">
+		Contact
+		</button>
+
+		<!-- Modal -->
+			<div class="modal" id="myModal" tabindex="1" role="dialog" aria-labelledby="myModalLabel">
+			  <div class="modal-dialog" role="document">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			        <h4 class="modal-title" id="myModalLabel">Contact</h4>
+			      </div>
+			      <div class="modal-body">
+			          <div class="container">
+						<form class="form-horizontal">
+						  	<div class="form-group">
+						    		<label for="inputNom" class="col-sm-2 control-label">Nom :</label>
+						    	<div class="col-sm-2">
+						      		<input type="text" class="form-control" id="inputNom" placeholder="Nom">
+						    	</div>
+						  	</div>
+					  		<div class="form-group">
+					    		<label for="inputPrenom" class="col-sm-2 control-label">Prenom :</label>
+					    		<div class="col-sm-2">
+					      			<input type="text" class="form-control" id="inputPrenom" placeholder="Prenom">
+					    		</div>
+					  		</div>
+					  		<div class="form-group">
+					    		<label for="inputMail" class="col-sm-2 control-label">Email :</label>
+					    		<div class="col-sm-2">
+					      			<input type="email" class="form-control" id="inputMail" placeholder="Email">
+					    		</div>
+					  		</div>
+						</div>
+			      </div> <!-- end of modal body -->
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			        <button type="button" class="btn btn-primary">Envoyer</button>
+			      </div>
+			  		</form>
+			    </div>
+			  </div>
+			</div>
+	    </div>
 	  </div><!-- /.container-fluid -->
 	</nav>
 
 		
 
-		<section  class="text-center">
-			<?= $this->section('main_content') ?>
-		</section>
+	<section  class="text-center">
+		<?= $this->section('main_content') ?>
+	</section>
 
-	<div class="container-fluid">
-		<div class="row footer">
-			<div class="col-md-8">
-				<p class="text-center">Created by Canivez Samuel, Deville Halim, Desramé Thibaut</p>
-			</div>
-			<div class="col-md-2 scrollinside">
-				<?php 
-				require "/../Controller/TwitterController.php";
-				?>
-			</div>
-		</div>
-	</div>
+<?php 
+	// require "/../Controller/TwitterController.php";
+?>
+	
 	
 
 	<script src="<?= $this->assetUrl("vendor/jquery/dist/jquery.min.js") ?>"></script>
