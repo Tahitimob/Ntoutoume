97d3d5bc7b0bc8a26d552c36b246f65602a6c332
Aequin0x <thibaut.desrame@gmail.com>
2016-08-16 14:23:04 +0200

    +

--- app/Views/user/inscription.php
+++ app/Views/user/inscription.php
@@ -2,21 +2,35 @@
 
 <?php $this->start('main_content') ?>
 
-<div id="inscription">
+<div id="inscription" class="mef">
 	<div class="container">
-	<h2>Inscrivez-vous dès maintenant</h2>
+	<h2 class="text-color">Inscrivez-vous dès maintenant</h2>
 	<div class="form-group col-md-6 col-md-offset-3">
 		<form method="POST">
-
-			<label for="username">Nom d'utilisateur :</label>
-			<input type="text" class="form-control" name="username" placeholder="Nom d'utilisateur">
-
-			<label for="password">Password :</label>
-			<input type="password" class="form-control" name="password" placeholder="Mot de passe">
+			<div class="form-group">
+				<label for="username">Nom d'utilisateur :</label>
+				<input type="text" class="form-control" name="username" placeholder="Nom d'utilisateur">
+			</div>
+			<div class="form-group">
+				<label for="password">Email :</label>
+				<input type="password" class="form-control" name="password" placeholder="Mot de passe">
+			</div>
+			<div class="form-group">
+				<label for="password">Password :</label>
+				<input type="password" class="form-control" name="password" placeholder="Mot de passe">
+			</div>
+			<div class="form-group">
+				<label for="password">Password :</label>
+				<input type="password" class="form-control" name="password" placeholder="Mot de passe">
+			</div>
 
 		<button name="register">S'inscire</button>
 		</form>
 	</div>
+	</div>
+	<div class="wrap">
+		<div id="b"></div>
+		<div class="vertical-line"></div>
 	</div>
 </div>
 <?php $this->stop('main_content') ?>
