28acb9715cfa32e4ea822da47c0a75a2755fdb29
Aequin0x <thibaut.desrame@gmail.com>
2016-08-17 16:46:02 +0200

    add modal info + change modal color

--- app/Views/pixel/create.php
+++ app/Views/pixel/create.php
@@ -27,16 +27,15 @@
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
-              <div class="modal-header">
+              <div class="modal-header modal-style">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="myModalLabel">Modal title</h4>
               </div>
-              <div class="modal-body">
-                ...
+              <div class="modal-body modal-style">
+                
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+              <div class="modal-footer modal-style">
+                <button type="button" class="btn btn-default btn-contact-modal" data-dismiss="modal">Ok !</button>
               </div>
             </div>
           </div>
