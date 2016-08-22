7bedcb2cc24877b5eaa0132f1e8e50df3d596cc3
Aequin0x <thibaut.desrame@gmail.com>
2016-08-22 15:36:04 +0200

    +

--- app/Views/security/logout.php
+++ app/Views/security/logout.php
@@ -3,7 +3,6 @@
 <?php $this->start('main_content') ?>
 <div class="mef form">
   	<div class="container">
-	    <h2 class="text-color">Votre profil</h2>
 	    <div class="form-group col-md-6 col-md-offset-3">
 		    <form action="<?= $this->url('security_logout') ?>" method="POST">
 		    	<h3 class="text-color">Etes vous sur de vouloir vous déconnecter ?</h3>
