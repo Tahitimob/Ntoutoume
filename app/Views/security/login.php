9a6d92d094943f3ad5c47f6a85af81f11e018371
tahitimob <toast.1234@laposte.net>
2016-08-25 12:34:27 +0300

    up

--- app/Views/security/login.php
+++ app/Views/security/login.php
@@ -3,19 +3,19 @@
 <?php $this->start('main_content') ?>
 <div class="mef form">
   <div class="container-fluid">
-    <h2 class="text-color">Sign in now</h2>
+    <h2 class="text-color">Connectez-vous dès maintenant</h2>
     <div class="form-group col-md-6 col-md-offset-3">
       <form method="POST" action="<?= $this->url('security_login') ?>">
 
         <div class="form-group">
-          <label for="username">Login :</label>
+          <label for="username">Nom d'utilisateur :</label>
           <input type="text" class="form-control" name="username" id="username" placeholder="Nom d'utilisateur">
         </div>
         <div class="form-group">
-          <label for="password">Password :</label>
+          <label for="password">Mot de passe :</label>
           <input type="password" class="form-control" name="password" placeholder="Mot de passe" id="password">
         </div>
-       <button type="submit" class="btn btn-default btn-inscription" name="security_login">Sign in</button>
+       <button type="submit" class="btn btn-default btn-inscription" name="security_login">Se connecter</button>
       <?php
         if (isset($error)) {
           echo "<br>";
