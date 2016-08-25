962f6173d10692731d1c12346f51a5365b1eb540
Aequin0x <thibaut.desrame@gmail.com>
2016-08-25 15:30:54 +0200

    403 / 404

--- app/Views/pixel/404.php
+++ app/Views/pixel/404.php
@@ -4,6 +4,6 @@
 
 <h1 class="text-color">404. Perdu ?</h1>
 
-<img src="<?= $this->assetUrl('img/shall-not-pass.gif')?>"/>
+<img src="<?= $this->assetUrl('img/trav.gif')?>"/>
 
 <?php $this->stop('main_content'); ?>
