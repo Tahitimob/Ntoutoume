8c0fc71bc7965ed4f466f32932a98711670419a0
Aequin0x <thibaut.desrame@gmail.com>
2016-08-25 15:26:31 +0200

    THE LAST SHIP

--- app/Views/pixel/404.php
+++ app/Views/pixel/404.php
@@ -1,7 +1,7 @@
 <?php $this->layout('layout', ['title' => "404 - Pix'Hell"]) ?>
 
 <?php $this->start('main_content'); ?>
-<hr class="hr">
+
 <h1 class="text-color">404. Perdu ?</h1>
 
 <img src="<?= $this->assetUrl('img/shall-not-pass.gif')?>"/>
--- app/Views/w_errors/403.php
+++ app/Views/w_errors/403.php
@@ -2,7 +2,7 @@
 
 
 <?php $this->start('main_content'); ?>
-<hr class="hr">
+
 <h1 class="text-color">403. Nothing to see here!</h1>
 
 <img src="<?= $this->assetUrl('img/shall-not-pass.gif')?>"/>
